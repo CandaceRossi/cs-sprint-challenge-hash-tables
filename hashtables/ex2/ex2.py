51a7e9a117c1a4eb06ce6f44a763953c19c91165
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2020-08-18 14:02:08 -0400

    return bool for pos nums and check if key is in dict with absolute val, then append neg val to result array

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,15 +10,37 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    solution = {}
+    l = len(tickets)
     route = []
     source = " "
     destination = " "
-    for source, destination in tickets:
-        if tickets[source] == None:
-            tickets[source].items()
-    print(tickets[source])
-        # elif:
-        #     tickets[destination +1]
-        # else route.append(tickets[destination] == None)
-    
+    for ticket in tickets:
+        if ticket not in solution:
+            solution[ticket] = []
+
+        if ticket.source == None:
+            route.append(ticket)
+        if ticket.source == None:
+            route.append(ticket)
+
+    for i in range(l - 1):
+        current_ticket = tickets[i]
+        route[current_ticket].append(tickets[i].destination)
+    #     if ticket.destination == None:
+    #         route.append(ticket.items())
+    #     else:
+    #         route.append(ticket.items())
+    # for i in range(l - 1):
+    #     current_ticket = ticket[i]
+    #     solution[current_ticket].append(ticket[i])
+    # for source, destination in tickets:
+    #     if tickets[source] == None:
+    #         route.append(tickets[source].items())
+    #     if
+    # print(ticket[source])
+    # elif:
+    #     tickets[destination +1]
+    # else route.append(tickets[destination] == None)
+
     return route
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,21 +3,23 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
-    result = {}
-    for num in a:
-        if num >= 0:
-            if num not in result:
-                result[num] = 1
-        if num < 0:
-            num = abs(num)
-            result[num] += 1
-    if result[num] == 2:
-        return result
+    posneg = {}
+    result = []
+    for nums in a:
+        if nums >= 0:
+            posneg[nums] = True
 
-# if __name__ == "__main__":
-#     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+    for nums in a:
+        if nums < 0 and abs(nums) in posneg:
+            result.append(-nums)
+
+    return result
 
 
-a = [-1, 2, 1, -2, 3, -4]
+if __name__ == "__main__":
+    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
 
-has_negatives(a)
+
+# a = [-1, 2, 1, -2, 3, -4]
+
+# has_negatives(a)
